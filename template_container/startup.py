2b034581f4644ed30d7adf2b9e0bffe4902a18c0
Hannes Karppila <hannes.karppila@gmail.com>
2018-02-15 10:26:56 +0200

    HACKFIX: "fix" a possible race condition with sleep

--- template_container/startup.py
+++ template_container/startup.py
@@ -28,6 +28,9 @@ if os.fork() == 0:
     commands[-1] += ["--replay-path", f"/replays/{gameid}_0.SC2Replay"]
     os.execlp("runuser", "-l", "user0", "-c", " && ".join(" ".join(shlex.quote(c) for c in cmd) for cmd in commands))
 else:
+    # HACK: Delay the joining client so the host has time to start up
+    import time; time.sleep(5)
+
     commands[-1] += ["--log-path", f"/replays/{gameid}_1.log"]
     commands[-1] += ["--replay-path", f"/replays/{gameid}_1.SC2Replay"]
     os.execlp("runuser", "-l", "user1", "-c", " && ".join(" ".join(shlex.quote(c) for c in cmd) for cmd in commands))
