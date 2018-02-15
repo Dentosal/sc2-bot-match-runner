bfe4f5e44f878a0ffcd785c219615bc73f3f79e4
Juha Paananen <juha.paananen@gmail.com>
2018-02-15 12:24:23 +0200

    download maps automatically too

--- downloadlinuxpackage.sh
+++ downloadlinuxpackage.sh
@@ -6,8 +6,24 @@ if [ -d StarCraftII ]; then
     echo "StarCraftII Linux binaries already present"
 else
     if [ ! -e "$ARCHIVE_NAME" ]; then
+        echo "Downloading StarCraft II Linux binaries"
         curl -O $ARCHIVE_URL
     fi
     unzip -P iagreetotheeula $ARCHIVE_NAME
     rm $ARCHIVE_NAME
+fi
+
+MAPS_NAME=Ladder2017Season3
+MAPS_FILE=Ladder2017Season3.zip
+MAPS_URL="http://blzdistsc2-a.akamaihd.net/MapPacks/$MAPS_FILE"
+
+if [ -d StarCraftII/Maps/$MAPS_NAME ]; then
+    echo "Maps already loaded in StarCraftII/Maps/$MAPS_NAME"
+else
+    if [ ! -e "$MAPS_FILE" ]; then
+        echo "Downloading maps from $MAPS_URL"
+        curl -O $MAPS_URL
+    fi
+    unzip -P iagreetotheeula $MAPS_FILE -d StarCraftII/Maps
+    rm $MAPS_FILE
 fi
