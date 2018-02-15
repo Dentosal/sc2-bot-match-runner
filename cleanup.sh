3ca81235ee00f4ba4b44fc1933892801cf5f45ce
Juha Paananen <juha.paananen@gmail.com>
2018-02-15 12:38:47 +0200

    cleanup stopped containers and untagged images too

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 docker ps | grep sc2_ | awk '{print $1}' | xargs docker kill
+
+docker ps -aq --no-trunc | xargs docker rm
+
+docker image ls|grep "<none>"|awk '{print $3}'|xargs docker image rm
