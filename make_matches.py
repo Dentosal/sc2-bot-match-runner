7be8271d042fc8cb89a9b3b19c92225c5ad36ccb
Hannes Karppila <hannes.karppila@gmail.com>
2018-02-16 09:24:24 +0200

    Remove duplicates from round-robin

--- make_matches.py
+++ make_matches.py
@@ -28,7 +28,7 @@ if args.type == "pairs":
 elif args.type == "round-robin":
     matches = []
     for i1, p1 in enumerate(args.participants):
-        for i2, p2 in enumerate(args.participants):
+        for i2, p2 in enumerate(args.participants[i1:]):
             if i1 != i2:
                 matches.append([p1, p2])
 
